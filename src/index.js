b5e803aa71b7edcc94a12a9e6010c3dd1ef9a8c7
Siigmaf <faby.tbe@gmail.com>
2023-06-23 16:51:13 -0300

    feat: Created PUT /users/id route

--- src/index.js
+++ src/index.js
@@ -23,6 +23,18 @@ app.get('/users', (req, res)=> {
     return res.status(200).json(users)
 });
 
+app.put('/users/:id', (req, res)=>{
+    const {id} = req.params;
+    console.log("🚀 ~ file: index.js:28 ~ app.put ~ id:", id);
+
+    const {name} = req.body
+    const findUser = users.find(user => user.id === id);
+    findUser.name = name;
+
+    return res.json(users);
+    
+})
+
 app.post('/books', (req, res)=>{
     const { name, author, company, description, user_id} = req.body;
     const id = uuid();
