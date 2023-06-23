d2fa0aca99bee6b4c04c1bf74ef5c5f78c13e6aa
Siigmaf <faby.tbe@gmail.com>
2023-06-23 16:40:41 -0300

    feat: Created POST /books route

--- src/index.js
+++ src/index.js
@@ -5,23 +5,29 @@ const app = express();
 const PORT = 3333;
 app.use(express.json());
 
-const users = []
+const users = [];
+const books = [];
 
 app.post('/users', (req, res)=>{
 const {name, email} = req.body;
 const id = uuid();
-
 const emailAlreadyExists = users.some((user)=> user.email === email)
 if(emailAlreadyExists){
     return res.status(400).json({message: "User Already exists"})
 }
-
 users.push({name,email,id});
 return res.status(201).json({message: "User created"})
 });
 
 app.get('/users', (req, res)=> {
     return res.status(200).json(users)
+});
+
+app.post('/books', (req, res)=>{
+    const { name, author, company, description, user_id} = req.body;
+    const id = uuid();
+    books.push({name, author, company, description, id, user_id})
+    return res.status(201).json({message: " Book created "})
 })
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
