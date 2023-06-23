f1f8089e16c8e7e6e5d903fb426f79cab6d2d1e1
Siigmaf <faby.tbe@gmail.com>
2023-06-23 16:04:08 -0300

    feat: Created POST /users route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const {v4: uuid} = require('uuid')
 
 const app = express();
 const PORT = 3333;
 app.use(express.json());
+
+const users = []
+
+app.post('/users', (req, res)=>{
+const {name, email} = req.body;
+const id = uuid();
+
+const emailAlreadyExists = users.some((user)=> user.email === email)
+if(emailAlreadyExists){
+    return res.status(400).json({message: "User Already exists"})
+}
+
+users.push({name,email,id});
+return res.status(201).json({message: "User created"})
+});
 
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
