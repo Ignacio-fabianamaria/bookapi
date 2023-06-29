bf986caa18565ca3bfcfe6c78d0f7a278c7dad2c
Siigmaf <faby.tbe@gmail.com>
2023-06-29 10:18:43 -0300

    feat: Add repositories folder for managing database access

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,37 +1,37 @@
 import { NextFunction, Request, Response } from 'express'
 import { User } from '../models'
 import { hash } from 'bcrypt'
+import { UserRepository } from '../repositories/UserRepository'
 
 class UserController{
-	index(req: Request, res:Response, next: NextFunction){
-	// buscar todos
-	}
-
-	show(req: Request, res:Response, next: NextFunction){
-	// buscar apenas um
+	private userRepository: UserRepository
+	constructor(){
+		this.userRepository = new UserRepository()
 	}
 
 	async store(req: Request, res:Response, next: NextFunction){
-	// criar
 		const {name, password, email} = req.body
 		try {
-			const findUser = await User.findOne({email})
-			console.log('🚀 ~ file: UserController.ts:18 ~ UserController ~ store ~ findUser:', findUser)
+			const findUser = await this.userRepository.findByEmail(email)
 			if(findUser){
 				throw new Error('User already exists')
 			}
 			const hashPassword = await hash(password, 10)
-			const createUser = await User.create({name, password: hashPassword, email})
+			const createUser = await this.userRepository.create({name, password:hashPassword, email })
 			return res.json(createUser)
 		} catch (error) {
 			next(error)
 		}
 	}
-
+	index(req: Request, res:Response, next: NextFunction){
+	// buscar todos
+	}
+	show(req: Request, res:Response, next: NextFunction){
+	// buscar apenas um
+	}
 	update(req: Request, res:Response, next: NextFunction){
 	// atualizar
 	}
-
 }
 
 export {UserController}
---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,25 @@
+import { User } from '../models'
+interface ICreate {
+	name:string,
+	email:string,
+	password:string
+}
+
+//Responsabilidade de se comunicar com o banco de dados
+class UserRepository{
+	async findByEmail(email:string){
+		const result = await User.findOne({email})
+		return result
+	}
+
+	async create({name, password,email}: ICreate){
+		const result = await User.create({
+			name,
+			password,
+			email
+		})
+		return result
+	}
+}
+
+export {UserRepository}
